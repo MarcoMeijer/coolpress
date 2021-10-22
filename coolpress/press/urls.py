3235281462d601a19d346c10f7c44d7173eb8292
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-10-22 13:50:37 +0200

    Login page

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:post_id>', views.post_detail, name='post-detail'),
     path('posts/', views.posts, name='posts'),
-    path('categories/', views.categories, name='categories')
+    path('categories/', views.categories, name='categories'),
+    path('accounts/', include('django.contrib.auth.urls'), name='accounts')
 ]
