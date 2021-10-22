f34cfb01ad154a9220b00c6bcc7befe40d97ec03
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-10-22 10:38:46 +0200

    Lesson 4

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:post_id>', views.post_detail, name='post_detail'),
+    path('posts/', views.posts, name='posts')
 ]
--- coolpress/press/views.py
+++ coolpress/press/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from press.models import Post, PostStatus
+
+
 def index(request):
     return HttpResponse("<b>Hello world!</b>")
+
+def get_html_from_post(post):
+    return f'''
+        <a href="/post/{post.pk}">
+            <h1>{post.title}</h1>
+        </a>
+        <h4>written by {post.author.user.username}</h4>
+        <p>{post.body}</p>
+        <p>{post.category.label}</p>
+        <p>{post.last_update}</p>
+    '''
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post_detail.html', {'post_obj': post})
+
+def posts(request):
+    post_list = Post.objects.filter(status=PostStatus.PUBLISHED.value).order_by('-pk')[:10]
+    return render(request, 'post_list.html', {'post_list': post_list})
